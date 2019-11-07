ba7f2a514cc4add4d8e39fa8c8848c5957994432
Peter K <pkjar16@student.aau.dk>
2019-11-07 10:58:52 +0100

    continued work

--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,91 @@
 import threading
+import datetime
+import bcw_task
+import ocw_task
+import pcp_task
+import ppc_task
+import sbm_task
+import time
+
 
 class Scheduler(threading.Thread):
-
-    def __init__(self):
+    def __init__(self, task_lst):
+        self.task_lst = task_lst
         threading.Thread.__init__(self)
+        threading.Thread.start(self)
 
-    def isHighestPriority(task_list, task):
-        increment = len(task_list)
-        while (increment > 0):
-            increment -= 1
-            highest_prio_task = task_list[0]
-            for item in task_list:
-                if item.priority < highest_prio_task.priority:
-                    highest_prio_task = item
-        return highest_prio_task
+    def run(self) -> None:
+        while True:
+            timeslice(self.task_lst)
+
+
+
+def get_time_ms():
+    return int(datetime.datetime.timestamp(datetime.datetime.now()) * 1000)
+
+
+def isHighestPriority(task_list, task):
+    increment = len(task_list)
+    while (increment > 0):
+        increment -= 1
+        highest_prio_task = task_list[0]
+        for item in task_list:
+            if item.priority < highest_prio_task.priority:
+                highest_prio_task = item
+    return highest_prio_task
+
+
+def timeslice(task_list):
+
+    # Sets the default task to be the lowest
+    active_task = SBM
+
+    while (True):
+        # check if SBM is the highest prio -> Yes
+        task_to_run = isHighestPriority(task_list, active_task)
+        if (active_task != task_to_run):
+            print("SBM is not the highest prio anymore")
+            active_task = task_to_run
+            # TODO kill current thread
+            # TODO start thread with task (task_to_run)
+        if (active_task == task_to_run):
+            print("Active Task is the highest prio")
+        time.sleep(1)
+
+
+def taskDone(task_list, task):
+    task_list.remove(task)
+    task_list = sorted(task_list, key=lambda task: task.priority, reverse=True)
+    return task_list
+
+def taskAppend(task_list, task):
+    task_list.append(task)
+    task_list = sorted(task_list, key=lambda task: task.priority, reverse=True)
+    return task_list
+
+
+
+
+# Priority is scheduled from incrementally, from 100 being the lowest and 500 being the highest.
+# TODO : Add each at the end of each task.
+# Back Wheel Task (multiple cards)
+BCW = bcw_task.BCW_Task(get_time_ms, 1000, 500)
+OCW = ocw_task.OCW_Task(get_time_ms, 1000, 400)  # Front Wheel Task (one card)
+PPC = ppc_task.PPC_Task(get_time_ms)  # Communication wth Raspberry Pi
+PCP = pcp_task.PCP_Task(get_time_ms, 1000, 200)  # Piston to push the card
+SBM = sbm_task.SBM_Task(get_time_ms, 1000, 100)  # Sliding the box in position
+asd = bcw_task.BCW_Task(get_time_ms, 1000, 5000)  # test
+asd2 = bcw_task.BCW_Task(get_time_ms)  # test
+
+# Add all the Tasks to a list, in the order of the highest being the first to be ran (highest priority) - may as well sort it manually
+task_list = []
+task_list.append(SBM)
+task_list.append(PCP)
+task_list.append(PPC)
+task_list.append(OCW)
+task_list.append(asd)
+task_list.append(asd2)
+task_list.append(BCW)
+
+# Launches the loop, ticking every time slice
+timeslice(task_list)
--- task.py
+++ task.py
@@ -1,4 +1,5 @@
 import threading
+import time
 
 class Task(threading.Thread):
 
@@ -7,14 +8,29 @@ class Task(threading.Thread):
         self.startTime = startTime
         self.deadline = deadline
         self.priority = priority
-        self.is_running = False
-
-    def start_task(self):
         self.is_running = True
         threading.Thread.start(self)
+      
+    def run(self) -> None:
+      while (True):
+        while (not self.is_running):
+          #Do stuff here
+          print("done")
+          return
 
-    def block_task(self):
-        self.is_running = False
+    def stop_task(self):
+      self.is_running = False
 
-    def resume_task(self):
-        self.is_running = True
+class OCW_Task(Task):
+    def __init__(self, startTime, deadline=1000, priority=9000):
+        super().__init__(startTime, deadline, priority)
+
+import datetime
+import time
+def get_time_ms():
+    return int(datetime.datetime.timestamp(datetime.datetime.now()) * 1000)
+
+#OCW = OCW_Task(get_time_ms, 1000, 400)  # Front Wheel Task (one card)
+#OCW.test()
+qwe = Task(get_time_ms, 1000,400)
+qwe.test()
