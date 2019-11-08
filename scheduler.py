eb8282708131de6c2efefad507be31862b657eb1
Huntersz <37700694+Cklein17@users.noreply.github.com>
2019-11-08 11:06:03 +0100

    cleanup scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -16,7 +16,7 @@ class Scheduler(threading.Thread):
 
     def run(self) -> None:
         while True:
-            # timeslice(self.task_lst)
+            # TODO Add scheduler code
             print("asd")
 
 
@@ -24,72 +24,65 @@ def get_time_ms():
     return int(datetime.datetime.timestamp(datetime.datetime.now()) * 1000)
 
 
-def isHighestPriority(task_list, task):
+def is_highest_priority(task_list, task):
     increment = len(task_list)
-    while (increment > 0):
+    while increment > 0:
         increment -= 1
-        highest_prio_task = task_list[0]
+        highest_priority_task = task_list[0]
         for item in task_list:
-            if item.priority < highest_prio_task.priority:
-                highest_prio_task = item
-    return highest_prio_task
+            if item.priority < highest_priority_task.priority:
+                highest_priority_task = item
+    # returns null if task_list is empty, error is referenced before assignment
+    return highest_priority_task
 
 
-def hasExceededDeadline(task):
+def has_exceeded_deadline(task):
     deadline = task.startTime() + task.deadline
     return deadline > get_time_ms
 
 
-def taskDone(task_list, task):
+def remove_task(task_list, task):
     task_list.remove(task)
-    task_list = sorted(task_list, key=lambda task: task.priority, reverse=True)
-    return task_list
+    return sorted(task_list, key=lambda task: task.priority, reverse=True)
 
 
-def taskAppend(task_list, task):
+def append_task(task_list, task):
     task_list.append(task)
-    task_list = sorted(task_list, key=lambda task: task.priority, reverse=True)
-    return task_list
+    return sorted(task_list, key=lambda task: task.priority, reverse=True)
 
 
 # Priority is scheduled from incrementally, from 100 being the lowest and 500 being the highest.
 # TODO : Add each at the end of each task.
 # Back Wheel Task (multiple cards)
-BCW = bcw_task.BCW_Task(get_time_ms, 1000, 500)
-OCW = ocw_task.OCW_Task(get_time_ms, 1000, 400)  # Front Wheel Task (one card)
-PPC = ppc_task.PPC_Task(get_time_ms)  # Communication wth Raspberry Pi
-PCP = pcp_task.PCP_Task(get_time_ms, 1000, 200)  # Piston to push the card
-SBM = sbm_task.SBM_Task(get_time_ms, 1000, 100)  # Sliding the box in position
-asd = bcw_task.BCW_Task(get_time_ms, 1000, 5000)  # test
-asd2 = bcw_task.BCW_Task(get_time_ms)  # test
+BCW = bcw_task.BCWTask(get_time_ms, 1000, 500)
+OCW = ocw_task.OCWTask(get_time_ms, 1000, 400)  # Front Wheel Task (one card)
+PPC = ppc_task.PPCTask(get_time_ms)  # Communication wth Raspberry Pi
+PCP = pcp_task.PCPTask(get_time_ms, 1000, 200)  # Piston to push the card
+SBM = sbm_task.SBMTask(get_time_ms, 1000, 100)  # Sliding the box in position
+asd = bcw_task.BCWTask(get_time_ms, 1000, 5000)  # test
+asd2 = bcw_task.BCWTask(get_time_ms)  # test
 
-# Add all the Tasks to a list, in the order of the highest being the first to be ran (highest priority) - may as well sort it manually
-task_list = []
-task_list.append(SBM)
-task_list.append(PCP)
-task_list.append(PPC)
-task_list.append(OCW)
-task_list.append(asd)
-task_list.append(asd2)
-task_list.append(BCW)
+# Add all the Tasks to a list, in the order of the highest being the first to be ran (highest priority) - may as well
+# sort it manually
+task_list = [SBM, PCP, PPC, OCW, asd, asd2, BCW]
 
 # Sets the default task to be the lowest
 active_task = SBM
-while (True):
+while True:
     print("active_task : " + str(active_task))
-    # check if SBM is the highest prio -> Yes
-    task_to_run = isHighestPriority(task_list, active_task)
-    if (active_task != task_to_run):
+    # check if SBM is the highest priority -> Yes
+    task_to_run = is_highest_priority(task_list, active_task)
+    if active_task != task_to_run:
         print("SBM is not the highest prio anymore")
         active_task = task_to_run
 
-    if (active_task == task_to_run):
+    if active_task == task_to_run:
         print("Active Task is the highest prio")
         # Check if the deadline has exceeded, kills the thread if it has
         # Run the tasks if it is not running
-        if (not active_task.is_running):
-            if (active_task.task_completed == True):
-                task_list = taskDone(task_list, active_task)
+        if not active_task.is_running:
+            if active_task.task_completed:
+                task_list = remove_task(task_list, active_task)
             active_task.start_task()
 
     time.sleep(1)
